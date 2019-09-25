3f0aaf312d7cb5c595fbc2b5940546d2b18fa371
Jonathan Reifer <jonathanr@osrce.com>
2019-09-24 23:46:44 -0400

    further cleanup of init setup

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -38,27 +38,32 @@ else
 	cd $DOWNLOAD_PATH/graphsense-transformation
 	git pull
 fi
-#docker build -t graphsense-transformation .
-cd $DOWNLOAD_PATH
-git clone https://github.com/JonathanReifer/graphsense-glue.git
-cd $DOWNLOAD_PATH/graphsense-glue
-#docker build -t graphsense-glue .
-cd $DOWNLOAD_PATH
-git clone https://github.com/graphsense/graphsense-dashboard.git graphsense-dashboard
+if [ ! -d $DOWNLOAD_PATH/graphsense-glue ]; then
+	git clone https://github.com/JonathanReifer/graphsense-glue.git
+else
+	cd $DOWNLOAD_PATH/graphsense-glue
+	git pull
+fi
 #cd $DOWNLOAD_PATH/graphsense-dashboard
 #git build -t graphsense-dashboard .
-cd $DOWNLOAD_PATH
-git clone https://github.com/graphsense/graphsense-REST.git graphsense-rest
-cp $BASE_PATH/template_configs/config.json graphsense-rest/app/
+if [ ! -d $DOWNLOAD_PATH/graphsense-rest ]; then
+	git clone https://github.com/graphsense/graphsense-REST.git graphsense-rest
+	cp $BASE_PATH/template_configs/config.json graphsense-rest/app/
+fi
 #cd $DOWNLOAD_PATH/graphsense-rest
 #git build -t graphsense-rest .
-cd $DOWNLOAD_PATH
-git clone https://github.com/graphsense/graphsense-blocksci.git graphsense-blocksci
+if [ ! -d $DOWNLOAD_PATH/graphsense-blocksci ]; then
+	git clone https://github.com/graphsense/graphsense-blocksci.git graphsense-blocksci
+fi
 #cd $DOWNLOAD_PATH/graphsense-blocksci 
-#git build -t graphsense-blocksci .
+#git build -t graphsense-blocksci .ii
 cd $DOWNLOAD_PATH
-git clone https://github.com/graphsense/btc-client.git graphsense-btc-client
-#cd $DOWNLOAD_PATH/graphsense-btc-client 
+if [ ! -d $DOWNLOAD_PATH/graphsense-btc-client ]; then
+	git clone https://github.com/graphsense/btc-client.git graphsense-btc-client
+else 
+	cd $DOWNLOAD_PATH/graphsense-btc-client 
+	git pull
+fi
 #git build -t graphsense-btc-client .
 #git clone https://github.com/graphsense/bch-client.git graphsense-bch-client
 #git clone https://github.com/graphsense/zec-client.git graphsense-zec-client
